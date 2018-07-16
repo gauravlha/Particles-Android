33c27207477795545afef0a51ab271cfe86572ed
Gaurav Lohia <gaurav.lha@gmail.com>
2018-07-16 21:07:15 +0530

    Snow Particles added

--- particles-android/src/main/java/com/gauravlha/particles_android/CustomParticlesView.java
+++ particles-android/src/main/java/com/gauravlha/particles_android/CustomParticlesView.java
@@ -9,42 +9,17 @@ import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.gauravlha.particles_android.model.ParticlesElements;
+import com.gauravlha.particles_android.model.ParticlesProperties;
+
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class CustomParticlesView extends View {
 
-    private int frameRate;
-    private int numberOfBalls;
+    private ParticlesProperties particlesProperties;
+    private ParticlesElements particlesElements;
 
-    private int xVelocityLowerLimit;
-    private int xVelocityUpperLimit;
-    private int yVelocityLowerLimit;
-    private int yVelocityUpperLimit;
-
-    private int paintAlphaLowerLimit;
-    private int paintAlphaUpperLimit;
-
-    private int ballSizeLowerLimit;
-    private int ballSizeUpperLimit;
-
-    private int lineXCoordinateDistance;
-    private int lineYCoordinateDistance;
-
-    private int circleColor;
-    private int lineColor;
-
-
-    private List<Integer> x;
-    private List<Integer> y;
-    private List<Paint> paint;
-    private List<Integer> paintAlpha;
-    private List<Integer> ballSize;
-    private List<Integer> xVelocity;
-    private List<Integer> yVelocity;
-    private List<Integer> randomXCoordinate;
-    private List<Integer> randomYCoordinate;
     private Context mContext;
     private Handler h;
     private Random random;
@@ -57,51 +32,54 @@ public class CustomParticlesView extends View {
 
     public CustomParticlesView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        particlesProperties = new ParticlesProperties();
+        particlesElements = new ParticlesElements();
         mContext = context;
         h = new Handler();
 
-        paint = new ArrayList<>();
-        paintAlpha = new ArrayList<>();
-        ballSize = new ArrayList<>();
-        x = new ArrayList<>();
-        y = new ArrayList<>();
-        xVelocity = new ArrayList<>();
-        yVelocity = new ArrayList<>();
-        randomXCoordinate = new ArrayList<>();
-        randomYCoordinate = new ArrayList<>();
+
+        particlesElements.setPaint(new ArrayList<Paint>());
+        particlesElements.setPaintAlpha(new ArrayList<Integer>());
+        particlesElements.setBallSize(new ArrayList<Integer>());
+        particlesElements.setX(new ArrayList<Integer>());
+        particlesElements.setY(new ArrayList<Integer>());
+        particlesElements.setxVelocity(new ArrayList<Integer>());
+        particlesElements.setyVelocity(new ArrayList<Integer>());
+        particlesElements.setRandomXCoordinate(new ArrayList<Integer>());
+        particlesElements.setRandomYCoordinate(new ArrayList<Integer>());
         random = new Random();
 
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.CustomParticlesView, 0, 0);
         try {
-            frameRate = ta.getInteger(R.styleable.CustomParticlesView_frameRate, 5);
-            numberOfBalls = ta.getInteger(R.styleable.CustomParticlesView_numberOfBalls, 50);
-            xVelocityLowerLimit = ta.getInteger(R.styleable.CustomParticlesView_xVelocityLowerLimit, 1);
-            xVelocityUpperLimit = ta.getInteger(R.styleable.CustomParticlesView_xVelocityUpperLimit, 5);
-            yVelocityLowerLimit = ta.getInteger(R.styleable.CustomParticlesView_yVelocityLowerLimit, 1);
-            yVelocityUpperLimit = ta.getInteger(R.styleable.CustomParticlesView_yVelocityUpperLimit, 5);
-            paintAlphaLowerLimit = ta.getInteger(R.styleable.CustomParticlesView_paintAlphaLowerLimit, 100);
-            paintAlphaUpperLimit = ta.getInteger(R.styleable.CustomParticlesView_paintAlphaUpperLimit, 255);
-            ballSizeLowerLimit = ta.getInteger(R.styleable.CustomParticlesView_ballSizeLowerLimit, 5);
-            ballSizeUpperLimit = ta.getInteger(R.styleable.CustomParticlesView_ballSizeUpperLimit, 10);
-            lineXCoordinateDistance = ta.getInteger(R.styleable.CustomParticlesView_lineXCoordinateDistance, 100);
-            lineYCoordinateDistance = ta.getInteger(R.styleable.CustomParticlesView_lineYCoordinateDistance, 20);
-            circleColor = ta.getColor(R.styleable.CustomParticlesView_circleColorName, getResources().getColor(android.R.color.black));
-            lineColor = ta.getColor(R.styleable.CustomParticlesView_lineColorName, getResources().getColor(android.R.color.holo_green_dark));
+            particlesProperties.setFrameRate(ta.getInteger(R.styleable.CustomParticlesView_frameRate, 5));
+            particlesProperties.setNumberOfBalls(ta.getInteger(R.styleable.CustomParticlesView_numberOfBalls, 50));
+            particlesProperties.setxVelocityLowerLimit(ta.getInteger(R.styleable.CustomParticlesView_xVelocityLowerLimit, 1));
+            particlesProperties.setxVelocityUpperLimit(ta.getInteger(R.styleable.CustomParticlesView_xVelocityUpperLimit, 5));
+            particlesProperties.setyVelocityLowerLimit(ta.getInteger(R.styleable.CustomParticlesView_yVelocityLowerLimit, 1));
+            particlesProperties.setyVelocityUpperLimit(ta.getInteger(R.styleable.CustomParticlesView_yVelocityUpperLimit, 5));
+            particlesProperties.setPaintAlphaLowerLimit(ta.getInteger(R.styleable.CustomParticlesView_paintAlphaLowerLimit, 100));
+            particlesProperties.setPaintAlphaUpperLimit(ta.getInteger(R.styleable.CustomParticlesView_paintAlphaUpperLimit, 255));
+            particlesProperties.setBallSizeLowerLimit(ta.getInteger(R.styleable.CustomParticlesView_ballSizeLowerLimit, 5));
+            particlesProperties.setBallSizeUpperLimit(ta.getInteger(R.styleable.CustomParticlesView_ballSizeUpperLimit, 10));
+            particlesProperties.setLineXCoordinateDistance(ta.getInteger(R.styleable.CustomParticlesView_lineXCoordinateDistance, 100));
+            particlesProperties.setLineYCoordinateDistance(ta.getInteger(R.styleable.CustomParticlesView_lineYCoordinateDistance, 20));
+            particlesProperties.setCircleColor(ta.getColor(R.styleable.CustomParticlesView_circleColorName, getResources().getColor(android.R.color.black)));
+            particlesProperties.setLineColor(ta.getColor(R.styleable.CustomParticlesView_lineColorName, getResources().getColor(android.R.color.holo_green_dark)));
 
         } finally {
             ta.recycle();
         }
 
-        for (int i = 0; i < numberOfBalls; i++) {
-            x.add(-1);
-            y.add(-1);
-            xVelocity.add(random.nextInt(xVelocityUpperLimit - xVelocityLowerLimit) + xVelocityLowerLimit);
-            yVelocity.add(random.nextInt(yVelocityUpperLimit - yVelocityLowerLimit) + yVelocityLowerLimit);
-            paint.add(new Paint());
-            paintAlpha.add(random.nextInt(paintAlphaUpperLimit - paintAlphaLowerLimit) + paintAlphaLowerLimit);
-            ballSize.add(random.nextInt(ballSizeUpperLimit - ballSizeLowerLimit) + ballSizeLowerLimit);
-            randomXCoordinate.add(random.nextBoolean() ? 1: -1);
-            randomYCoordinate.add(random.nextBoolean() ? 1: -1);
+        for (int i = 0; i < particlesProperties.getNumberOfBalls(); i++) {
+            particlesElements.getX().add(-1);
+            particlesElements.getY().add(-1);
+            particlesElements.getxVelocity().add(random.nextInt(particlesProperties.getxVelocityUpperLimit() - particlesProperties.getxVelocityLowerLimit()) + particlesProperties.getxVelocityLowerLimit());
+            particlesElements.getyVelocity().add(random.nextInt(particlesProperties.getyVelocityUpperLimit() - particlesProperties.getyVelocityLowerLimit()) + particlesProperties.getyVelocityLowerLimit());
+            particlesElements.getPaint().add(new Paint());
+            particlesElements.getPaintAlpha().add(random.nextInt(particlesProperties.getPaintAlphaUpperLimit() - particlesProperties.getPaintAlphaLowerLimit()) + particlesProperties.getPaintAlphaLowerLimit());
+            particlesElements.getBallSize().add(random.nextInt(particlesProperties.getBallSizeUpperLimit() - particlesProperties.getBallSizeLowerLimit()) + particlesProperties.getBallSizeLowerLimit());
+            particlesElements.getRandomXCoordinate().add(random.nextBoolean() ? 1 : -1);
+            particlesElements.getRandomYCoordinate().add(random.nextBoolean() ? 1 : -1);
         }
 
 
@@ -109,45 +87,44 @@ public class CustomParticlesView extends View {
 
     protected void onDraw(Canvas c) {
 
-        for (int i = 0; i < numberOfBalls; i++) {
-            if (x.get(i) < 0 && y.get(i) < 0) {
-                x.set(i, random.nextInt(this.getWidth()));
-                y.set(i, random.nextInt(this.getHeight()));
+        for (int i = 0; i < particlesProperties.getNumberOfBalls(); i++) {
+            if (particlesElements.getX().get(i) < 0 && particlesElements.getY().get(i) < 0) {
+                particlesElements.getX().set(i, random.nextInt(this.getWidth()));
+                particlesElements.getY().set(i, random.nextInt(this.getHeight()));
             } else {
-                x.set(i, x.get(i) + (xVelocity.get(i)) * randomXCoordinate.get(i));
-                y.set(i, y.get(i) + (yVelocity.get(i)) * randomYCoordinate.get(i));
-                if ((x.get(i) > this.getWidth() - ballSize.get(i)) || (x.get(i) < 0)) {
-                    xVelocity.set(i, xVelocity.get(i) * -1);
+                particlesElements.getX().set(i, particlesElements.getX().get(i) + (particlesElements.getxVelocity().get(i)) * particlesElements.getRandomXCoordinate().get(i));
+                particlesElements.getY().set(i, particlesElements.getY().get(i) + (particlesElements.getyVelocity().get(i)) * particlesElements.getRandomYCoordinate().get(i));
+                if ((particlesElements.getX().get(i) > this.getWidth() - particlesElements.getBallSize().get(i)) || (particlesElements.getX().get(i) < 0)) {
+                    particlesElements.getxVelocity().set(i, particlesElements.getxVelocity().get(i) * -1);
                 }
-                if ((y.get(i) > this.getHeight() - ballSize.get(i)) || (y.get(i) < 0)) {
-                    yVelocity.set(i, yVelocity.get(i) * -1);
+                if ((particlesElements.getY().get(i) > this.getHeight() - particlesElements.getBallSize().get(i)) || (particlesElements.getY().get(i) < 0)) {
+                    particlesElements.getyVelocity().set(i, particlesElements.getyVelocity().get(i) * -1);
                 }
 
             }
 
-            paint.get(i).setStyle(Paint.Style.FILL);
-            paint.get(i).setColor(Color.WHITE);
-            paint.get(i).setAlpha(paintAlpha.get(i));
-            c.drawPaint(paint.get(i));
+            particlesElements.getPaint().get(i).setStyle(Paint.Style.FILL);
+            particlesElements.getPaint().get(i).setColor(Color.WHITE);
+            particlesElements.getPaint().get(i).setAlpha(particlesElements.getPaintAlpha().get(i));
+            c.drawPaint(particlesElements.getPaint().get(i));
 
 
         }
 
-        for (int i = 0; i < numberOfBalls; i++) {
-            paint.get(i).setColor(circleColor);
-            c.drawCircle(x.get(i), y.get(i), ballSize.get(i), paint.get(i));
-            for (int j = 0; j < numberOfBalls; j++) {
-                int valueX = Math.abs(x.get(i) - x.get(j));
-                int valueY = Math.abs(y.get(i) - y.get(j));
-                if (valueX < lineXCoordinateDistance || (valueY < lineYCoordinateDistance && valueX < lineXCoordinateDistance)) {
-                    paint.get(i).setColor(lineColor);
-                    c.drawLine(x.get(i), y.get(i), x.get(j), y.get(j), paint.get(i));
+        for (int i = 0; i < particlesProperties.getNumberOfBalls(); i++) {
+            particlesElements.getPaint().get(i).setColor(particlesProperties.getCircleColor());
+            c.drawCircle(particlesElements.getX().get(i), particlesElements.getY().get(i), particlesElements.getBallSize().get(i), particlesElements.getPaint().get(i));
+            for (int j = 0; j < particlesProperties.getNumberOfBalls(); j++) {
+                int valueX = Math.abs(particlesElements.getX().get(i) - particlesElements.getX().get(j));
+                int valueY = Math.abs(particlesElements.getY().get(i) - particlesElements.getY().get(j));
+                if (valueX < particlesProperties.getLineXCoordinateDistance() || (valueY < particlesProperties.getLineYCoordinateDistance() && valueX < particlesProperties.getLineXCoordinateDistance())) {
+                    particlesElements.getPaint().get(i).setColor(particlesProperties.getLineColor());
+                    c.drawLine(particlesElements.getX().get(i), particlesElements.getY().get(i), particlesElements.getX().get(j), particlesElements.getY().get(j), particlesElements.getPaint().get(i));
                 }
             }
 
         }
 
-
-        h.postDelayed(r, frameRate);
+        h.postDelayed(r, particlesProperties.getFrameRate());
     }
 }
